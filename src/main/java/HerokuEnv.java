4b1577ceb90240e626b292cafbf89e8aed5afe56
Denis Kozarenko <denis_kozarenko@ukr.net>
2023-01-06 13:42:57 +0200

    port changed

--- src/main/java/HerokuEnv.java
+++ src/main/java/HerokuEnv.java
@@ -3,7 +3,7 @@ public class HerokuEnv {
         try {
             return Integer.parseInt(System.getenv("PORT"));
         } catch (NumberFormatException ex) {
-            return 3500;
+            return 5000;
         }
     }
 }
